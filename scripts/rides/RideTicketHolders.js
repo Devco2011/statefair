e305e3da46326c06d006b64169ae773b5c196366
Devco2011 <devco2011@gmail.com>
2020-09-04 15:26:05 -0500

    Counting Tickets

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,11 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", rideEvent => {
+        if (rideEvent.detail.ticketPurchased !== "0") {
+            contentTarget.innerHTML += `<div class="rider person"></div>`
+
+        }
     })
 }
 
